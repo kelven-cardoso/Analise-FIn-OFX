830b50c320f9ac5a1957f543d436289364fdb2a4
Kelven Cardoso <84426581+kelven-cardoso@users.noreply.github.com>
2025-01-29 17:51:51 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -11,7 +11,6 @@ df["Mês"] = df["Data"].apply(lambda x: "-".join(x.split("-")[:-1]))
 df["Data"] = pd.to_datetime(df["Data"])
 df["Data"] = df["Data"].apply(lambda x: x.date())
 df = df[df["Categoria"]!="Receitas"]
-df["Valor"] = 1
 
 def filter_data(df, mes, selected_categories):
     df_filtered = df[df['Mês'] == mes]
